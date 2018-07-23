5062e65b98852dd096e45b309e3a35e6dd4441d3
Indira Williams <indirajamurath@gmail.com>
2018-07-23 15:14:25 -0400

    worked on tasklister with chris

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,23 @@
 
-document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
-});
+document.addEventListener("DOMContentLoaded", init)
+
+const submitForm = document.querySelector('#create-task-form')
+const taskList = document.querySelector('#tasks')
+const taskField = document.querySelector('[name="new-task-description"]')
+
+function init(){
+  submitForm.addEventListener('submit', addTask)
+  function addTask(e){
+    e.preventDefault()
+    const taskInfo = taskField.value
+      const template = `<li>${taskInfo}<button class="delete">x</button></li>`
+      taskList.innerHTML += template
+      submitForm.reset();
+  }
+
+  document.body.addEventListener('click', function(e){
+    if (e.target.className === 'delete'){
+       e.target.parentElement.remove();
+    }
+  })
+}
--- src/task.js
+++ src/task.js
@@ -1,2 +1,10 @@
+//What does a task have?
+// - a template
+// - input data
+// - delete button
+
 class Task {
+  constructor(description) {
+  this.description = description
+  }
 }
